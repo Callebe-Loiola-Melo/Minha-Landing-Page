a9dcf854c9efefea0a05346dee1cba719f11006e
Callebe Loiola Melo <callebeloiola2007@gmail.com>
2025-03-31 00:33:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Botão do WhatsApp
+document.getElementById("whatsapp-btn").addEventListener("click", function() {
+    window.location.href = "https://api.whatsapp.com/send?phone=61995103430";
+});
+
+// Botão para iniciar conversa no Instagram (DM)
+document.getElementById("dm-btn").addEventListener("click", function() {
+    window.location.href = "https://www.instagram.com/direct/t/17842375014450836";
+});
+
+// Botão do GitHub
+document.getElementById("github-btn").addEventListener("click", function(event) {
+    window.open("https://github.com/Callebe-Loiola-Melo", "_blank");
+});
+
+// Botão do Instagram para projetos
+document.getElementById("instagram-projetos-btn").addEventListener("click", function(event) {
+    window.open("https://www.instagram.com/clm_web_studio/", "_blank");
+});
+
